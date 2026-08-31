a6baf02e136c647327c36a41dde8ddbb798c3952
sahiljag <sahiljagtap9666@gmail.com>
2026-08-31 21:08:09 +0530

    change application.py

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,53 +1,44 @@
-import os
-import sys
-import pandas as pd
+from flask import Flask,request,render_template
 import numpy as np
-from src.logger import logging
-from src.exception import CustomException
-from src.utils import load_object
+import pandas as pd
+import sys
+import traceback
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import PredictPipeline,CustomData
 
+application = Flask(__name__)
 
-class PredictPipeline:
-    def __init__(self):
-        pass 
+app = application
 
-    def predict(self,features):
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('home.html')
+    else:
         try:
-            model_path = os.path.join('artifacts','model.pkl')
-            preprocessor_path = os.path.join('artifacts','preprocessor.pkl')
-            model = load_object(file_path=model_path)
-            preprocessor = load_object(file_path=preprocessor_path)
-            data_scaled = preprocessor.transform(features)
-            pred = model.predict(data_scaled)
-            return pred
-        except Exception as e:
-            raise CustomException(e,sys)
+            data = CustomData(gender=request.form.get('gender'),
+                    race_ethnicity=request.form.get('ethnicity'),
+                    parental_level_of_education=request.form.get('parental_level_of_education'),
+                    lunch=request.form.get('lunch'),
+                    test_preparation_course=request.form.get('test_preparation_course'),
+                    reading_score=float(request.form.get('reading_score')),
+                    writing_score=float(request.form.get('writing_score'))
+                )
+            pred_df = data.get_data_as_df()
+            print(pred_df, flush=True)
 
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
+            return render_template('home.html',results=results[0])
+        except Exception:
+            print("PREDICTION ERROR:", flush=True)
+            traceback.print_exc(file=sys.stdout)
+            sys.stdout.flush()
+            raise
 
-
-class CustomData:
-    def __init__(self,gender:str,race_ethnicity:str,parental_level_of_education:str,lunch: str,test_preparation_course: str,reading_score: int,writing_score: int):
-        self.gender = gender
-        self.race_ethnicity = race_ethnicity
-        self.parental_level_of_education = parental_level_of_education
-        self.lunch = lunch
-        self.test_preparation_course = test_preparation_course
-        self.reading_score = reading_score
-        self.writing_score = writing_score
-    def get_data_as_df(self):
-            try:
-                custom_data_input_dict = {
-                    "gender": [self.gender],
-                    "race_ethnicity": [self.race_ethnicity],
-                    "parental_level_of_education": [self.parental_level_of_education],
-                    "lunch": [self.lunch],
-                    "test_preparation_course": [self.test_preparation_course],
-                    "reading_score": [self.reading_score],
-                    "writing_score": [self.writing_score],
-                        }
-            
-                return pd.DataFrame(custom_data_input_dict)
-            
-            except Exception as e:
-                    raise CustomException(e, sys)
-        
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
